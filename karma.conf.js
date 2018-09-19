4c39b66165a15e03816b05b92fd7f3c6f6a7f72d
Lars Langhans <lars.langhans@hypoport.de>
2018-09-19 12:52:32 +0200

    version updates

--- karma.conf.js
+++ karma.conf.js
@@ -4,30 +4,52 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: [
+      'jasmine',
+      '@angular/cli'
+    ],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require("karma-coverage"),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: [
+        'html',
+        'lcovonly'
+      ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: [
+      'dots',
+      'progress',
+      'kjhtml'
+    ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    singleRun: false,
     browsers: ['Chrome'],
-    singleRun: false
+    customLaunchers: {
+      ChromeKarma: {
+        base: "ChromeHeadless",
+        flags: [
+          "--lang=de-DE",
+          "--disable-gpu",
+          "--no-sandbox",
+          "--disable-web-security"
+        ]
+      }
+    }
   });
 };
